6d85ac971c6d970030720196d4cd6dd7dd2f69f6
AbdulRehman <likeam99@gmail.com>
2023-01-21 12:04:36 +0500

    fetch function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,10 @@
 import jsonPlaceholder from "../api/jsonPlaceholder";
 
 
-export const fetchPost = () => {
+export const fetchPost = () =>  async dispath => {
+        const response = await jsonPlaceholder.get('/posts')
 
-    const response =  jsonPlaceholder.get('/posts')
+    dispath({type: 'FETCH_POSTS', payload: response})
+    }
 
-    return{
-        type: 'FETCH_POSTS',  
-        payload: response
-   
-    };
-};
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react';
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
 
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store} >
     <App />
   </Provider >
 );
